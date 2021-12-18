eafdcd5a3cabaf32c35575a01f2aa42ce3b17ece
micavanco <micavanco@gmail.com>
2021-12-18 16:31:46 +0100

    feat(order-list): add animations to order list

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useReducer, useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 import './table.scss';
 import { Order } from "../../models/order.model";
 import { OrderList } from "../order-list/order-list";
@@ -11,16 +12,33 @@ interface TableProps {
 }
 
 export const Table: React.FC<TableProps> = ({ bidList, askList, cryptoCurrency, currency  }) => {
+  const config = {
+    from: { opacity: 0, transform: "scale(0)" },
+    to: { opacity: 1, transform: "scale(1)" },
+    duration: 800
+  };
+  const [styles, api] = useSpring(() => config);
+
+
+  useEffect(() => {
+    api({ opacity: 0, transform: "scale(0)" });
+    setTimeout(() => {
+      api({ opacity: 1, transform: "scale(1)" });
+    }, 800);
+  }, [cryptoCurrency, currency]);
 
 
   return (
     <div className="table">
-      <div className="table__bid">
-        <OrderList title={'Bid'} list={bidList} cryptoCurrency={cryptoCurrency} currency={currency}/>
+      <animated.div className="table__bid" style={styles}>
+        <OrderList title={'Bid'} list={bidList} cryptoCurrency={cryptoCurrency}
+                   currency={currency}/>
+      </animated.div>
+      <div className="table__separator">
       </div>
-      <div className="table__ask">
+      <animated.div className="table__ask" style={styles}>
         <OrderList title={'Ask'} list={askList} cryptoCurrency={cryptoCurrency} currency={currency}/>
-      </div>
+      </animated.div>
     </div>
   );
 }
